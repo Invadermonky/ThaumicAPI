72ee823fc3bdc5c3059dcfb6cda204ddd9e98028
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-03-02 00:08:40 -0800

    add movement ability handler class

---
+++ src/main/java/com/invadermonky/thaumicapi/api/handlers/CommonEventHandler.java
@@ -0,0 +1,36 @@
+package com.invadermonky.thaumicapi.api.handlers;
+
+import com.invadermonky.thaumicapi.ThaumicAPIMod;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid = ThaumicAPIMod.MOD_ID)
+public class CommonEventHandler {
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public static void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
+        if (event.getEntityLiving() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+            if (PlayerMovementAbilityHandler.isValidSideForMovement(player)) {
+                PlayerMovementAbilityHandler.tick(player);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onJump(LivingEvent.LivingJumpEvent event) {
+        if (event.getEntityLiving() instanceof EntityPlayer && PlayerMovementAbilityHandler.isValidSideForMovement((EntityPlayer) event.getEntityLiving())) {
+            PlayerMovementAbilityHandler.onJump((EntityPlayer) event.getEntityLiving());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        if (event.getEntity() instanceof EntityPlayer && PlayerMovementAbilityHandler.isValidSideForMovement((EntityPlayer) event.getEntity())) {
+            PlayerMovementAbilityHandler.onPlayerRecreation((EntityPlayer) event.getEntity());
+        }
+    }
+}
