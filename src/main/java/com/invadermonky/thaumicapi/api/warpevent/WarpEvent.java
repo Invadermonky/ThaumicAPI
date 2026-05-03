238dbdca308a7349d20d3f301f06e0b4a929eb43
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-05-02 18:44:19 -0700

    add javadoc

--- src/main/java/com/invadermonky/thaumicapi/api/warpevent/WarpEvent.java
+++ src/main/java/com/invadermonky/thaumicapi/api/warpevent/WarpEvent.java
@@ -1,10 +1,16 @@
 package com.invadermonky.thaumicapi.api.warpevent;
 
+import com.invadermonky.thaumicapi.warpevents.WarpEventRegistry;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * A functional interface used to define {@link IWarpEvent} classes for automatic registration
+ * with {@link WarpEventRegistry}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface WarpEvent {
