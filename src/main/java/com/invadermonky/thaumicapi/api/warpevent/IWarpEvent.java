0c4a57bfc4ddb67550cdbcfe068233129b5d3c25
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-05-04 10:53:14 -0700

    improve javadocs

--- src/main/java/com/invadermonky/thaumicapi/api/warpevent/IWarpEvent.java
+++ src/main/java/com/invadermonky/thaumicapi/api/warpevent/IWarpEvent.java
@@ -8,42 +8,74 @@ import thaumcraft.common.config.ModConfig.CONFIG_GRAPHICS;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+/**
+ * An interface used to create new Thaumcraft 6 warp events.
+ */
 public interface IWarpEvent {
-    /** The unique resource location identifier of this warp event. */
+    /**
+     * The unique resource location identifier of this warp event.
+     */
     @Nonnull
     ResourceLocation getEventName();
 
-    /** The minimum amount of warp required for this event to occur. This value is exclusive. */
+    /**
+     * The minimum amount of player warp required for this event to occur. This value is exclusive.
+     */
     int getMinimumWarp();
 
-    /** The maximum amount of warp allowed for this event to occur. This value is inclusive. */
+    /**
+     * The maximum amount of player warp allowed for this event to occur. This value is inclusive.
+     */
     default int getMaximumWarp() {
         return Integer.MAX_VALUE;
     }
 
+    /**
+     * The weight of this warp event. Higher weight will increase the chance of this event
+     * occurring. Base Thaumcraft events have a default weight of 10.
+     */
     int getEventWeight();
 
-    /** Play the warp event sound. This method is only fired client-side. */
+    /**
+     * Play the warp event sound. This method is only fired client-side.
+     */
     void playClientEventSound(EntityPlayer player, int warp);
 
-    /** The warp event message. */
+    /**
+     * Returns the warp event message sent to the player when this event fires.
+     */
     @Nullable
     ITextComponent getEventMessage(EntityPlayer player, int warp);
 
-    /** Runs the warp event. This method will fire server-side first, then client-side. */
+    /**
+     * Runs the warp event. This is where all warp event processing should occur.
+     * This method will fire server-side first, then client-side.
+     */
     void performWarpEvent(EntityPlayer player, int warp);
 
-    /** Checks if this event should fire. This method only fires server-side */
+    /**
+     * Checks if this event should fire. This can be used to create event conditionals
+     * such as research requirements or location checks. This method fires server-side.
+     */
     default boolean shouldEventProcess(EntityPlayer player) {
         return true;
     }
 
-    /** Whether this event will be disabled by the {@link CONFIG_GRAPHICS#nostress} setting. */
+    /**
+     * Whether this event will be disabled by the {@link CONFIG_GRAPHICS#nostress} setting.
+     * This setting is primarily used to disable events that may cause anxiety for users
+     * such as the heartbeat sound effect or creeper fuse igniting.
+     */
     default boolean isStressful() {
         return false;
     }
 
-    /** Whether this warp event is enabled. This should be used for configuration toggles. */
+    /**
+     * Whether this warp event is enabled. Returning false with this method will fully
+     * disable the event, such as with configuration toggles.
+     * <p>
+     * For event conditionals, use {@link IWarpEvent#shouldEventProcess(EntityPlayer)}.
+     */
     default boolean isEnabled() {
         return true;
     }
--- src/main/java/com/invadermonky/thaumicapi/api/warpevent/WarpEvent.java
+++ src/main/java/com/invadermonky/thaumicapi/api/warpevent/WarpEvent.java
@@ -10,6 +10,9 @@ import java.lang.annotation.Target;
 /**
  * A functional interface used to define {@link IWarpEvent} classes for automatic registration
  * with {@link WarpEventRegistry}.
+ * <p>
+ * You can use this interface for automatic event registering,
+ * or add the event manually via {@link WarpEventRegistry#registerWarpEvent(IWarpEvent)}.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
